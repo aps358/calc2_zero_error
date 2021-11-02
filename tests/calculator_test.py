5583cad897670e8cbecacf1a9762df609c2026c6
aps358 <as358@njit.edu>
2021-11-02 02:41:22 -0400

    Division Updated

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -34,5 +34,5 @@ def test_calculator_multiply():
 def test_calculator_divide():
     """ tests multiplication of two numbers"""
     calc = Calculator()
-    result  = calc.multiply_numbers(1,0)
+    result  = calc.divide_numbers(1,0)
     assert result == 0.5
